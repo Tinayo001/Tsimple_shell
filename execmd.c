d7889533be9b7eb2913f353479bbb850e13d7d12
kepo402 <alawodeolayinka2@gmail.com>
2024-02-27 06:58:43 -0800

    betty updated

--- execmd.c
+++ execmd.c
@@ -4,16 +4,16 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+
 /**
  * execmd - executes a command
  * @argv: pointer to a pointer of arguments
  */
-
 void execmd(char **argv)
 {
-        if (execvp(argv[0], argv) == -1)
-        {
-                perror("execvp");
-                exit(EXIT_FAILURE);
-        }
+	if (execvp(argv[0], argv) == -1)
+	{
+		perror("execvp");
+		exit(EXIT_FAILURE);
+	}
 }
