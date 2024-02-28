2e88bf611388d3ef0d780e1b2bae425a19914ec8
kepo402 <alawodeolayinka2@gmail.com>
2024-02-28 12:21:20 -0800

    main entry updated

--- main.c
+++ main.c
@@ -5,16 +5,42 @@
  *
  * Return: Always 0.
  */
-int main(void)
+int main(int argc, char *argv[])
 {
-	char command[MAX_COMMAND_LENGTH];
+    char command[MAX_COMMAND_LENGTH];
 
-	while (1)
-	{
-		display_prompt();
-		read_command(command);
-		execute_command(command);
-	}
+    if (argc == 1)
+    {
+        /* Interactive mode */
+        while (1)
+        {
+            display_prompt();
+            read_command(command);
+            execute_command(command);
+        }
+    }
+    else if (argc == 2)
+    {
+        /* Non-interactive mode */
+        FILE *file = fopen(argv[1], "r");
+        if (file == NULL)
+        {
+            perror("Error opening file");
+            return (EXIT_FAILURE);
+        }
 
-	return (0);
+        while (fgets(command, MAX_COMMAND_LENGTH, file) != NULL)
+        {
+            execute_command(command);
+        }
+
+        fclose(file);
+    }
+    else
+    {
+        fprintf(stderr, "Usage: %s [file]\n", argv[0]);
+        return (EXIT_FAILURE);
+    }
+
+    return (EXIT_SUCCESS);
 }
